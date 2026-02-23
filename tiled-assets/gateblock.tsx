c4f0e1c64f2045c70d35d62027318ef97524d1f9
Raymond Liu <raymondliu4414@gmail.com>
2026-02-22 16:16:55 -0800

    Added gate assets

---
+++ tiled-assets/gateblock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="gateblock" tilewidth="80" tileheight="80" tilecount="8" columns="2">
+ <image source="../src/assets/gateblock.png" width="160" height="320"/>
+</tileset>
