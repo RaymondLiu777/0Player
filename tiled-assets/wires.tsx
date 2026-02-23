9d6ea8a835ffaacbfb044f784bf6a98dcf84c974
Raymond Liu <raymondliu4414@gmail.com>
2026-02-22 23:09:52 -0800

    Added new wires and fixed issue with up/down of gate not initializing

--- tiled-assets/wires.tsx
+++ tiled-assets/wires.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="wires" tilewidth="64" tileheight="64" tilecount="36" columns="4">
- <image source="../src/assets/wires.png" width="256" height="576"/>
+<tileset version="1.10" tiledversion="1.11.2" name="wires" tilewidth="64" tileheight="64" tilecount="56" columns="8">
+ <image source="../src/assets/wires.png" width="512" height="448"/>
 </tileset>
