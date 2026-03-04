f3cbbde014320364db79162ae62d8fa73c2b150a
Raymond Liu <raymondliu4414@gmail.com>
2026-03-04 00:53:16 -0800

    Make canvas fullscreen and add menu/settings overlay

--- src/js/game.js
+++ src/js/game.js
@@ -5,8 +5,12 @@ const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
 // Set canvas size
-canvas.width = 800;
-canvas.height = 600;
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
 
 // Camera position
 let cameraX = 0;
